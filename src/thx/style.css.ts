c533cc24cce7d6a16b08a381d87a0472fb45b88a
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-05-30 14:39:06 +0300

    init

--- src/thx/style.css.ts
+++ src/thx/style.css.ts
@@ -1,16 +1,17 @@
 import { style } from "@vanilla-extract/css";
 
 const container = style({
-  display: "flx",
+  display: "flex",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
   padding: "0 1rem",
   textAlign: "center",
+  height: "100vh",
 });
 
 const rocket = style({
-  marginTop: "5rem",
+  // marginTop: "5rem",
 });
 
 export const thxSt = {
